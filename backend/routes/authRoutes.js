5a68af694afde6b6dc8216b13a1dde71353d2b3e
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-11 17:51:13 +0530

    Updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,26 +1,12 @@
 import express from "express";
-// Import the express library to create the server
 import { getMe, login, logout, signup } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/protectRoute.js";
-// Import the login and signup functions from the authController file
 
 const router = express.Router();
-// Create a new Express router instance
 
 router.get("/me", protectRoute, getMe);
-
-// Define a POST route for the signup endpoint
-// This route will handle user signup requests
 router.post("/signup", signup);
-
-// Define a POST route for the login endpoint
-// This route will handle user login requests
 router.post("/login", login);
-
-// Define a POST route for the logout endpoint
-// This route will handle user logout requests
 router.post("/logout", logout);
 
-// Export the router instance as the default export
 export default router;
-// Export the router instance so it can be used in other parts of the application
