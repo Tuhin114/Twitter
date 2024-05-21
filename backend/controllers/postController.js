5e23cd6cf080e960a49d5e06ce6367ee743ca824
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-21 20:32:38 +0530

    updated

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,5 +1,5 @@
-import Post from "../models/post.model.js";
-import User from "../models/user.model.js";
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
 import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
